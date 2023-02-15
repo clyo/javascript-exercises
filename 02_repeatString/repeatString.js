e6ffc0f8ac0be6d427a0a3ce3cd79ade09fe900e
Clyo <colm-l@hotmail.com>
2023-02-15 18:28:52 -0500

    Updating javascript files
    Updating javascript files that contain the answers for the
    exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatString=""
+    if (num < 0){
+        errMsg = "ERROR";
+        return errMsg;
+    } else {
+        for (i = num; i > 0; i--){
+            concatString += string;
+    }
+    }
+return concatString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(array) {
+    var output = [];
+    for (var i=array.length-1; i>=0 ; i--){
+        output += array[i];
+    }
+    return output.toString();
 };
 
 // Do not edit below this line
